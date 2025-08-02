61e979ff25c920b10d1b0b30a145d91c4aa0edac
mym365 <3501709516@qq.com>
2025-08-02 16:15:41 +0800

    Update config.py

--- simulation/config.py
+++ simulation/config.py
@@ -1,1 +1,16 @@
-
+class SimConfig:
+    """simulation params"""
+    # data generation
+    n = 500  # sample size
+    J = 10   # motifs length
+    L = 15   # sequence length
+    p = 20   # motifs number
+    elements = "ACDEFGHIKLMNPQRSTVWY" #Amino acid type set
+    random_seed = 42
+    
+    # network params
+    input_dim = 200
+    hidden_dim1 = 60
+    hidden_dim2 = 20
+    lr = 0.05 
+    iter_num = 200
