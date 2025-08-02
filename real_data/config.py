39e88eacaab511f56ae5c61cc4029eb28cb6f331
mym365 <3501709516@qq.com>
2025-08-02 17:49:52 +0800

    Update config.py

--- real_data/config.py
+++ real_data/config.py
@@ -1,1 +1,13 @@
-
+class Config:
+    J = 9   # motifs length
+    p = 20   # motifs number
+    elements = "ACDEFGHIKLMNPQRSTVWY" #Amino acid type set
+    geno_num = 4
+    random_seed = 42
+    
+    # network params
+    input_dim = 180
+    hidden_dim1 = 60
+    hidden_dim2 = 20
+    lr = 0.05 
+    iter_num = 300
